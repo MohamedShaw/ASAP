dc8feb9588f2bcef34da05aec2594edd1ebf572c
Mohamed Shawky <m.shwaky@index-soft.com>
2021-12-21 21:02:45 +0200

    search

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -1,18 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import {View, RefreshControl} from 'react-native';
 import {styles} from './style';
-import {AppList, AppSpinner, showError, LIGHT_COLORS} from 'common';
+import {AppList, AppSpinner, showError, LIGHT_COLORS, AppInput} from 'common';
 
 import I18n from 'react-native-i18n';
 import {ProductCard} from '../productCard/ProductCard';
 import {EmptyList} from '../emptyList/EmptyList';
 import {getProductList} from 'api/ProductApi';
+import {debounce} from 'lodash';
+
 interface Props {}
 export const ProductList: React.FC<Props> = () => {
   const [loading, setLoading] = useState<Boolean>(true);
+  const [value, setValue] = useState('');
+  // const _onChangeSearchText = debounce(setValue, 1000);
 
   const [refreshing, setRefresh] = useState<Boolean>(false);
   const [data, setData] = useState<Array<[]>>([]);
+  console.log('data', data);
+
   let page = React.useRef(1);
 
   const getProducts = async () => {
@@ -47,34 +53,52 @@ export const ProductList: React.FC<Props> = () => {
     ) : null;
   };
 
+  const filteredData = data.filter((item) =>
+    item.category.toString().toLowerCase().includes(value.trim().toLowerCase()),
+  );
+
   return (
-    <AppList
-      data={data}
-      renderItem={({item, index}) => <ProductCard data={item} />}
-      onEndReached={handleMore}
-      style={styles.listContainer}
-      numColumns={2}
-      ListFooterComponent={<Footer />}
-      ListFooterComponentStyle={styles.spinnerContainer}
-      onEndReachedThreshold={0.2}
-      ItemSeparatorComponent={() => (
-        <View style={{height: 28, justifyContent: 'center'}}>
-          <View
-            style={{height: 2, alignSelf: 'stretch', backgroundColor: '#E3E3E3'}}
+    <>
+      <AppInput
+        style={{borderRadius: 25}}
+        containerStyle={{borderRadius: 25, height: 45}}
+        noValidation
+        multiline
+        value={value}
+        onChangeText={setValue}
+      />
+      <AppList
+        data={filteredData}
+        renderItem={({item, index}) => <ProductCard data={item} />}
+        onEndReached={handleMore}
+        style={styles.listContainer}
+        numColumns={2}
+        ListFooterComponent={<Footer />}
+        ListFooterComponentStyle={styles.spinnerContainer}
+        onEndReachedThreshold={0.2}
+        ItemSeparatorComponent={() => (
+          <View style={{height: 28, justifyContent: 'center'}}>
+            <View
+              style={{
+                height: 2,
+                alignSelf: 'stretch',
+                backgroundColor: '#E3E3E3',
+              }}
+            />
+          </View>
+        )}
+        refreshControl={
+          <RefreshControl
+            colors={[LIGHT_COLORS.primary]}
+            refreshing={refreshing}
+            onRefresh={() => {
+              setRefresh(true);
+              getProducts(1);
+            }}
           />
-        </View>
-      )}
-      refreshControl={
-        <RefreshControl
-          colors={[LIGHT_COLORS.primary]}
-          refreshing={refreshing}
-          onRefresh={() => {
-            setRefresh(true);
-            getProducts(1);
-          }}
-        />
-      }
-      ListEmptyComponent={!loading && data.length === 0 && <EmptyList />}
-    />
+        }
+        ListEmptyComponent={!loading && data.length === 0 && <EmptyList />}
+      />
+    </>
   );
 };
